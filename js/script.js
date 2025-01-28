7baac6f144f961b02fe9d35737a065cad76d17b1
Mikhail Arkhangelskii <arkh.m00@gmail.com>
2025-01-29 02:10:47 +0300

    Adopted carousels

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 const partnersSwiper = new Swiper(".swiper-partners", {
-  slidesPerView: 6,
-  spaceBetween: 90,
-  slidesPerGroup: 6,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  // autoplay: true,
-});
-
-const newsSwiper = new Swiper(".swiper-news", {
-  slidesPerView: 3,
-  spaceBetween: 250,
+  slidesPerView: 3, 
+  spaceBetween: 100,
   slidesPerGroup: 3,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 6,
+      spaceBetween:  90,
+      slidesPerGroup: 6,
+    },
+  },
+  autoplay: true,
+});
+
+const newsSwiper = new Swiper(".swiper-news", {
+  slidesPerView: 1, 
+  spaceBetween: 200,
+  slidesPerGroup: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 250,
+      slidesPerGroup: 3,
+    },
   },
 })
