3cf24d26b5c88f69e0ffd1e59a727becdd00bb62
Михаил Архангельский <hurricane.@MacBook-Air-Mihail.local>
2025-01-28 15:20:10 +0300

    Completed footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const partnersSwiper = new Swiper(".swiper-partners", {
   slidesPerView: 6,
   spaceBetween: 90,
-  loop: true,
+  slidesPerGroup: 6,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -12,7 +12,7 @@ const partnersSwiper = new Swiper(".swiper-partners", {
 const newsSwiper = new Swiper(".swiper-news", {
   slidesPerView: 3,
   spaceBetween: 250,
-  loop: true,
+  slidesPerGroup: 3,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
